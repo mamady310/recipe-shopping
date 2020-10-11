bddffd61ea19a2bf885ae0096b6a02036465d2dc
Mamady Sillah <supurpisces310@msn.com>
2020-10-10 22:53:49 -0400

    created shopping list and recipe book components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule,
