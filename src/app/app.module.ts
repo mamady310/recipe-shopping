eb7c5c1ef84325c1a33088710ed7b6b056150b53
Mamady Sillah <supurpisces310@msn.com>
2020-10-10 23:05:02 -0400

    created header folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
