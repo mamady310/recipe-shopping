028d63f13298d2f0297cfc321200f0c49d1ce07c
Mamady Sillah <supurpisces310@msn.com>
2020-10-11 15:11:05 -0400

    created recipe-list component within the recipees component

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The purpose of this project is for a user to add and edit recipes. In additon to this function a user will be able to create a shopping list based off of the recipes. 
 
-## Technolog Used
+## Technology Used
 
 Angular
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
-    RecipeBookComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
